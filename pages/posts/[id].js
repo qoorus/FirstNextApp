d6a0ee4650310293f0ee8e8c22980436b6bc9099
qoorus <q7254task@gmail.com>
2020-12-31 02:21:13 +0900

    render markdown

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -9,12 +9,14 @@ export default function Post({ postData }) {
             {postData.id}
             <br />
             {postData.date}
+            <br />
+            <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
         </Layout>
     )
 }
 
 export async function getStaticProps({ params }) {
-    const postData = getPostData(params.id)
+    const postData = await getPostData(params.id)
     return {
         props: {
             postData
